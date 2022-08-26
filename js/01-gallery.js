0a7b931030ae747d9ad439a0decf77795807341d
Aleksey Knigin <alexyo2626@gmail.com>
2022-08-27 00:49:46 +0300

    add task_1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const cardMurkup = createGalleryMurkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", cardMurkup);
+
+//рендер разметки
+function createGalleryMurkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}"> 
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+galleryEl.addEventListener("click", onCardClick);
+
+//открытие модального окна
+function onCardClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}">
+  `);
+
+  instance.show();
+
+  //   прослушка клавистуры и закрытие по Esc
+  window.addEventListener("keydown", closeModalWindowByEsc);
+
+  function closeModalWindowByEsc(event) {
+    console.log(event.code);
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", closeModalWindowByEsc);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const cardMurkup = createGalleryMurkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", cardMurkup);
+
+//рендер разметки
+function createGalleryMurkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </div>`;
+    })
+    .join("");
+}
+
+//подключение галереии
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
